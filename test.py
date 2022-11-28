6f86f2c5cefe2105919fea2be5ffa4bbe5b22eee
Punitsaini0-9 <punitsaini7900@gmail.com>
2022-11-28 13:56:26 +0530

    Faulty calculators.py

---
+++ test.py
@@ -0,0 +1,3 @@
+def punit():
+    x = input.store()
+    print(x)
---
+++ tow matrix add.py
@@ -0,0 +1,14 @@
+def matrix_1():
+
+    num1 = [[1,3,4],[1,3,5],[4,5,8]]
+    num2 = [[1,2,3],[4,6,5],[6,4,8]]
+
+    num3 = num1 + num2              
+
+    print(num3)
+
+    return(matrix_1)
+
+    
+
+matrix_1() 
