e029ab6c4a410ee660220c99ac7174a7c95fcdaa
Punitsaini0-9 <punitsaini7900@gmaIL.com>
2022-12-12 20:16:35 +0530

    globle scope and local scope

--- globel scope & local scope.py
+++ globel scope & local scope.py
@@ -1,3 +1,5 @@
+""""
+
 # # try 1
 
 # l = 10  #Globle variable
@@ -10,12 +12,39 @@
 
 # function1("This is am amd")
 
+"""
+
+
+"""
+
+#  # try 2
 l = 15 
 
 def function2(n):
+    # l = 5
+    m = 6
     global l #(this function allow for global variables and not allow for local varaibles)
-    m = l+45
+    l = l + 45
+    print(l , m)
+    print(n)
+function2("hello")
 
-    print(m)
+"""
 
-function()
+x = 89
+def punit():
+    x = 20
+
+    def prashant():
+
+        global x
+        x = 88
+
+    # print("before calling punit", x)
+
+    prashant()
+
+    print("after calling punit", x)
+
+punit()
+print(x)
