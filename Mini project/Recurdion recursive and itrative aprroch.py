bb9cc159a338c0cb86366da504085f73ed6b5bf6
Punitsaini0-9 <punitsaini7900@gmaIL.com>
2022-12-12 23:14:42 +0530

    Recursions_ Recursive Vs Iterative Approach

--- Mini project/Recurdion recursive and itrative aprroch.py
+++ Mini project/Recurdion recursive and itrative aprroch.py
@@ -33,6 +33,18 @@ def foctorial_recursive(n):
 
     else:
         return n * foctorial_recursive(n -1)
+        
+        """
+        How to work this function
+
+        some example the no. is 5:
+        5 * factorial_recusion(4)
+        5 * 4 * factorial_recursion(3)
+        5 * 4 * 3 * factorial_recusion(2)
+        5 * 4 * 3 * 2 * factorial_recusion(1)
+        5 * 4 * 3 * 2 * 1 = 120
+        
+        """
 
 number = int(input("Enter the number here\n"))
 print("Factorial using Rcorsive method", factorial_Itrative(number))
